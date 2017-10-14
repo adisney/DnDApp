ea9dfa5cab8d1f0e6286c7780cd109eacd99bc24
Seth <sjcohen2@gmail.com>
2017-10-13 23:03:25 -0700

    Update some tests and maybe fix stuff?

--- contracts/GMToken.sol
+++ contracts/GMToken.sol
@@ -9,12 +9,24 @@ contract GMToken {
 
     mapping (address => GM) GMs; 
 
-    function newGM(string name, int32 id) {
+    function newGM(string name, int32 id) public {
     require(GMs[tx.origin].id == 0);
     GMs[tx.origin] = GM(name, id);
     }
 
-    function hasToken() returns (bool) {
+    function hasToken() public returns (bool) {
         return GMs[tx.origin].id > 0;
+    }
+
+    function getName() public returns (string) {
+        GM storage gm = GMs[tx.origin];
+        require(gm.id != 0);
+        return gm.name;
+    }
+
+    function getID() public returns (int32) {
+        GM storage gm = GMs[tx.origin];
+        require(gm.id != 0);
+        return gm.id;
     }
 }
